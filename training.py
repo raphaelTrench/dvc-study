60bb53b8dbb0506c4ca9ccb58e14a46450fd0a85
rtrench <raphaeltrench@gmail.com>
2023-10-19 20:48:31 -0300

    dump

--- training.py
+++ training.py
@@ -4,6 +4,7 @@ import lightgbm
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from mlem.api import save
 
 X,y = load_iris(as_frame=True,return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
@@ -21,3 +22,7 @@ with Live("custom_dir") as live:
 
     # Log additional metrics after training
     live.log_metric("summary_metric", 1.0, plot=False)
+    
+
+# instead of joblib.dump(rf, "models/rf")
+save(lightgbm, "models/lgbm", sample_data=X_train)
